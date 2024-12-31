4786488a6b373f806c129bd07e3acf7d5bb4655f
Fabrizio1407 <43219818+Fabrizio1407@users.noreply.github.com>
2024-12-31 18:02:50 +0100

    Quasi completato da console

--- script.js
+++ script.js
@@ -18,9 +18,15 @@ const gameBoard = ( function() {
 
     const addSign = function(sign) {
         return function(x,y) {
-            board[x][y]= sign;
+            
             if (board[x][y]!=null)
-               return "Mossa non valida"         
+               {gameFlow.setChiTocca();
+                return "Mossa non valida" } 
+            else {
+                board[x][y]= sign;
+                return gameFlow.getTurno();
+            
+            }   
         }
 
     }
@@ -34,32 +40,110 @@ const gameBoard = ( function() {
 })();
 
 
-function player() {
+function player(nomeSel,sign) {
     let punteggio =0;
+    let mySign=sign;
     let risultato = "";
+    let nome=nomeSel;
+
+
+    const getNome = () => nome;
 
     const mettiX = gameBoard.addSign("X");
-    const mettiY = gameBoard.addSign("O");
+    const mettiO = gameBoard.addSign("O");
 
-    const makeMove = (move,x,y) => {
-        if (move=="X") {
+    const makeMove = (x,y) => {
+        if (mySign=="X") {
             risultato = mettiX(x,y)}
         else 
-         {risultato = mettiY(x,y)}
-
-        return risultato;
+         {risultato = mettiO(x,y)}
+         
+         
+        return !gameFlow.checkTris(gameBoard.getBoard())? risultato:gameFlow.checkFull==8?"PARI":"VINTO!";
     }
 
     const addScore = () => {punteggio++};
 
     
-
-    
-
+   return {makeMove, addScore, getNome}
 }
 
 
+const gameFlow =  (function() {
+    let chiTocca = 1;
+    let checkFull =0
 
-function gameFlow() {
     
+
+    const getTurno = () => {
+        if (chiTocca==1) {
+            let str = `Tocca a ${player1.getNome()}`
+            return str
+        }
+    else {
+        let str = `Tocca a ${player2.getNome()}`
+         return str;
+    }};
+
+    const getChiTocca = () => chiTocca;
+    const setChiTocca = () => chiTocca==1?chiTocca=2:chiTocca=1;
+
+    const checkTris = (board) => {
+        let end = false;
+        
+
+        for (let i=0;i<3;i++) {
+
+          
+            // righe
+            if ((board[i][0]===board[i][1] && board[i][1]===board[i][2] && board[i][0]!== null) )  end=true;
+            
+
+            // colonne
+            if ((board[0][i]===board[1][i] && board[1][i]===board[2][i] && board[0][i]!== null) )  end=true;
+
+             // diagonale 1
+             if ((board[0][0]===board[1][1] && board[1][1]===board[2][2] && board[0][0]!==null))  end=true;     
+             // diagonale 2
+             if ((board[0][2]===board[1][1] && board[1][1]===board[2][0] && board[0][2]!==null) )  end=true;
+
+    }
+
+      for (let i=0;i<3;i++) {
+        for (let j=0;j<3;j++) {
+            if (board[i][j]!=null)
+                checkFull++;
+            if (checkFull==8) end=true;
+        }
+      }
+      console.log(checkFull)
+
+    return end;
 }
+
+    return {checkTris, getTurno, getChiTocca,setChiTocca,checkFull}
+    
+}) ();
+
+const player1 = player(prompt("Giocatore 1 - Digita il tuo nome: "),"X");
+const player2 = player(prompt("Giocatore 2 - Digita il tuo nome: "),"O");
+
+ console.log(` Giocatore 1 = X  --- Giocatore 2 = O \n ${gameFlow.getTurno()}`);
+
+   while (!gameFlow.checkTris(gameBoard.getBoard()))
+
+    if (gameFlow.getChiTocca()==1) {
+        console.log(player1.makeMove(prompt(`${player1.getNome()} muove -->  Riga: `,0),prompt(`Colonna: `,0)));
+        console.log(gameBoard.getBoard());
+        gameFlow.setChiTocca();
+
+    } else
+    {
+        console.log(player2.makeMove(prompt(`${player2.getNome()} muove -->  Riga: `,0),prompt(`Colonna: `,0)));
+        console.log(gameBoard.getBoard());
+        gameFlow.setChiTocca();
+    }
+
+
+
+
